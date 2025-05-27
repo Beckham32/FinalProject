b264769f4049452c3a40de53e2dd83683cbba89c
Beckham <beckhamb1@educbe.ca>
2025-05-27 10:10:59 -0600

    tailwind

--- game/vite.config.js
+++ game/vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { config } from 'dotenv';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { config } from "dotenv";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   define: {
     "process.env": process.env,
   },
